f745252bd9cac939997e0f20bfa3e135054ff673
9P9 <59532064+9P9@users.noreply.github.com>
2021-05-21 23:20:43 +0100

    Update scraper.js

--- Source/scraper.js
+++ Source/scraper.js
@@ -3,7 +3,7 @@ const request = require('request');
 var scrapeProxies = new Promise((resolve, reject) => {
     request({
         method: "GET",
-        url: "https://www.proxy-list.download/api/v1/get?type=http",
+        url: " https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=2500&country=all&ssl=all&anonymity=all&simplified=true",
     }, (error, response, body) => {
         if (body) {
             return resolve(body.split("\n"));
